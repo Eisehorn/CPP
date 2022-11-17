e48f3c63a7481d6457d60e82c0576e4ef0c106ee
Francesco Giuliano <fgiulian@e4r10p1.42roma.it>
2022-11-17 12:55:11 +0100

    yay

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fgiulian <fgiulian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 15:20:19 by fgiulian          #+#    #+#             */
-/*   Updated: 2022/11/15 16:30:51 by fgiulian         ###   ########.fr       */
+/*   Updated: 2022/11/17 12:50:03 by fgiulian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,9 @@ int main() {
 	PhoneBook phonebook;
 	int i = 0;
 	int j = 0;
+	int k = 0;
 	std::string temp;
-	
+
 	while (j == 0) {
 		std::cout<<"Enter ADD, SEARCH or EXIT"<<std::endl;
 		std::getline(std::cin, temp);
@@ -33,8 +34,8 @@ int main() {
 			std::cout<<"Enter contact index"<<std::endl;
 			std::getline(std::cin, temp);
 			if (temp.length() == 1 && 8 > temp[0] - 48 && temp[0] - 48 >= 0) {
-				i = temp[0] - 48;
-				phonebook.phonebook_search(i);
+				k = temp[0] - 48;
+				phonebook.phonebook_search(k);
 			}
 			else
 				std::cout<<"Index not valid"<<std::endl;
