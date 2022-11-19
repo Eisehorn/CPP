6f93a989292c2c304ab6dd3a5f292eb55f59d4ab
Francesco Giuliano <fgiulian@e3r3p1.42roma.it>
2022-11-19 18:46:12 +0100

    ex02 done

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <iostream> 
+
+int main() {
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+	std::cout<<&str<<"; "<<static_cast<void*>(stringPTR)<<"; "<<&stringREF<<std::endl;
+	std::cout<<str<<"; "<<*stringPTR<<"; "<<stringREF<<std::endl;
+}
