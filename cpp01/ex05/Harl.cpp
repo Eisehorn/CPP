a00fa72a801181c9bc24949e5abc2bd397db6c7b
Eisehorn <eisehorn@MSI.localdomain>
2022-11-29 02:35:55 +0100

    ex05 almost done

---
+++ cpp01/ex05/Harl.cpp
@@ -0,0 +1,9 @@
+#include "Harl.hpp"
+
+Harl::Harl(){};
+
+Harl::~Harl(){};
+
+void	Harl::complain(std::string level) {
+	
+}
---
+++ cpp01/ex05/main.cpp
@@ -0,0 +1,10 @@
+#include "Harl.hpp"
+
+int main() {
+	Harl harl;
+	std::string debug = "debug";
+	std::string info = "info";
+	std::string warning = "warning";
+	std::string error = "error";
+	
+}
